307c75411420906b59e02dca2d6813b7b71f3684
makoto2025 <aratamakoto2025@gmail.com>
2026-08-04 15:55:19 -0700

    organize

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-	return <></>;
+	return (
+		<div className="min-h-screen bg-slate-50 flex items-center justify-center">
+			<p className="text-xl font-bold text-slate-800">Portfolio starting...</p>
+		</div>
+	);
 }
 
 export default App;
